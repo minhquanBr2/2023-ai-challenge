4b4bf3cdb098840c449bccd70353c92a2fcd0813
minhquanBr2 <minhquanhole03@gmail.com>
2023-09-19 09:46:35 +0700

    delete globallink

--- GlobalLink.py
+++
@@ -1,5 +0,0 @@
-VideosFolder = "E:\\2023 HCM AI CHALLENGE\\video"
-CsvFolder = "E:\\2023 HCM AI CHALLENGE\\map-keyframes"
-KeyframeFolder = "E:\\2023 HCM AI CHALLENGE\\Keyframes"
-ResultsCSV = "E:\\2023 HCM AI CHALLENGE\\queries-p3-result\\"
-
