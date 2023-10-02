34c6b8eeb0ffba03a86ed51ee1170c46f13b6ac6
Khang0608 <hoduykhanghocmai@gmail.com>
2023-10-03 00:41:08 +0700

    creat whoosh

--- ocr/search/whoosh-create-index.py
+++ ocr/search/whoosh-create-index.py
@@ -2,6 +2,7 @@ import os
 from whoosh.index import create_in
 from whoosh.fields import Schema, TEXT, ID
 from whoosh.analysis import RegexAnalyzer, LowercaseFilter, StopFilter, CharsetFilter
+from glob import glob
 
 
 def createAccentMap(path):
@@ -21,7 +22,7 @@ def createSearchableData(root, path):
     # https://stackoverflow.com/questions/42769299/whoosh-not-searching-words-with-accent
     accent_analyzer = RegexAnalyzer(r'\w+') | LowercaseFilter() \
                   | StopFilter() | CharsetFilter(charmap=charmap)
-    schema = Schema(title=TEXT(stored=True), path=ID(stored=True), 
+    schema = Schema(video_name=TEXT(stored=True), keyframe_idx=TEXT(stored=True), 
                     content=TEXT, textdata=TEXT(stored=True, analyzer=accent_analyzer))
     
     if not os.path.exists("indexdir"):
@@ -31,15 +32,16 @@ def createSearchableData(root, path):
     ix = create_in("indexdir", schema)
     writer = ix.writer()
  
-    filepaths = [os.path.join(root, i) for i in os.listdir(root)]
+    # filepaths = [os.path.join(root, i) for i in os.listdir(root)]
+    filepaths = glob('E:\\AIChallenge\\corpus\\*\\*.txt')
     for path in filepaths:
         fp = open(path, 'r', encoding="utf-8")
         print(path)
         text = fp.read()
-        writer.add_document(title=path.split("\\")[1], path=path, content=text, textdata=text)
+        writer.add_document(video_name=path.split("\\")[-2], keyframe_idx=path.split("\\")[-1][:-4], content=text, textdata=text)
         fp.close()
     writer.commit()
  
-root = "corpus"
-path = "D:/VSCode/2023-ai-challenge/ocr/search/vietnamese-dict.txt"
+root = "E:\\AIChallenge\\corpus"
+path = "D:\\University\\Contest\\AIChallenge\\2023-ai-challenge\\ocr\\search\\vietnamese-dict.txt"
 createSearchableData(root, path)
--- ocr/search/whoosh-search.py
+++ ocr/search/whoosh-search.py
@@ -1,6 +1,7 @@
 from whoosh.qparser import QueryParser
 from whoosh import scoring
 from whoosh.index import open_dir
+from whoosh.query import FuzzyTerm
 import sys
  
 ix = open_dir("indexdir")
@@ -10,10 +11,11 @@ query_str = sys.argv[1]
 # Top 'n' documents as result
 topN = int(sys.argv[2])
 
-searcher = ix.searcher(weighting=scoring.TF_IDF)
-query = QueryParser("content", ix.schema).parse(query_str)
+searcher = ix.searcher(weighting=scoring.Frequency)
+query = QueryParser("content", ix.schema, termclass=FuzzyTerm).parse(query_str)
 results = searcher.search(query)
-print(query)
+# print(query)
+print(results)
 
 for i in range(min(topN, len(results))):
-    print(results[i]['title'], str(results[i].score), results[i]['textdata'])
+    print(results[i]['video_name'], results[i]['keyframe_idx'], str(results[i].score), results[i]['textdata'])
