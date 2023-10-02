ac3e3cea26360b548734b17bff32a9322f67b37d
minhquanBr2 <minhquanhole03@gmail.com>
2023-10-03 00:59:49 +0700

    add whoosh search

--- ocr/search/whoosh-search.py
+++ ocr/search/whoosh-search.py
@@ -1,6 +1,7 @@
 from whoosh.qparser import QueryParser
 from whoosh import scoring
 from whoosh.index import open_dir
+from whoosh.query import FuzzyTerm
 import sys
  
 ix = open_dir("indexdir")
@@ -11,9 +12,9 @@ query_str = sys.argv[1]
 topN = int(sys.argv[2])
 
 searcher = ix.searcher(weighting=scoring.TF_IDF)
-query = QueryParser("content", ix.schema).parse(query_str)
+query = QueryParser("content", ix.schema, termclass=FuzzyTerm).parse(query_str)
 results = searcher.search(query)
-print(query)
+print(results)
 
 for i in range(min(topN, len(results))):
     print(results[i]['title'], str(results[i].score), results[i]['textdata'])
